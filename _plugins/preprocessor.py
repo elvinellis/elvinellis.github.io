0918f68bec0fed7b53d91b6dec6339b3b3721462
Elvin Chua <elvin.chua@outlook.com>
2016-10-09 04:05:47 +0000

    Updated baby page

--- _plugins/preprocessor.py
+++ _plugins/preprocessor.py
@@ -2,9 +2,11 @@ import fileinput
 import re
 
 doublequote = re.compile(r'(\s+)"([^"]*)"(\s+)', re.MULTILINE)
+singlequote = re.compile(r'(\s+)\'([^\']*)\'(\s+)', re.MULTILINE)
 apos = re.compile(r'([a-zA-Z0-9])\'([a-zA-Z\."\s])', re.MULTILINE)
 emdash = re.compile(r'([a-zA-Z0-9]*) \- ([a-zA-Z0-9]*)', re.MULTILINE)
-ellipsis = re.compile(r'([a-zA-Z0-9^.]*)\.\.\.([\s+|"])', re.MULTILINE)
+ellipsis = re.compile(r'([a-zA-Z0-9^.]*)\.\.\.([\s+|"|)])', re.MULTILINE)
+doubleEx = re.compile(r'\!\!([\s+|"|)])', re.MULTILINE)
 multiply = re.compile(r'([0-9]+) x ([0-9]+)', re.MULTILINE)
 fraction = re.compile(r'([0-9]+)/([0-9]+)', re.MULTILINE)
 ordinalFirst = re.compile(r'1st', re.MULTILINE)
@@ -16,7 +18,9 @@ for c in fileinput.input():
     if ".story" in c or c.strip().startswith('|') or c.strip().startswith('h2'):
         #print c
         t = ellipsis.sub(r"\1&hellip;\2", c)
+        t = doubleEx.sub(r"&#8252;\1", t)
         t = doublequote.sub(r"\1&ldquo;\2&rdquo;\3", t)
+        t = singlequote.sub(r"\1&lsquo;\2&rsquo;\3", t)
         t = apos.sub(r"\1&#8217;\2", t)
         t = emdash.sub(r"\1 &mdash; \2", t)
         t = multiply.sub(r"\1&times;\2", t)
